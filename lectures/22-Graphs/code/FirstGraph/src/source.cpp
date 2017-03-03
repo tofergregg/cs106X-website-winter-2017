474394de4bca46ee6a040b532650e3370135429f
Chris Gregg <tofergregg@gmail.com>
2017-03-03 08:46:28 -0800

    added lecture 22 Graphs materials

---
+++ lectures/22-Graphs/code/FirstGraph/src/source.cpp
@@ -0,0 +1,31 @@
+#include <cctype>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include "console.h"
+#include "filelib.h"
+#include "grid.h"
+#include "gwindow.h"
+#include "simpio.h"
+#include "pqueue.h"
+#include "basicgraph.h"
+
+using namespace std;
+
+
+int main() {
+    setConsoleFont("courier-24");
+
+    BasicGraph graph;
+    graph.addNode("A");
+    graph.addNode("B");
+    graph.addNode("C");
+
+    graph.addEdge("B", "A");
+    graph.addEdge("B", "C");
+    graph.addEdge("C", "B");
+
+    return 0;
+}
